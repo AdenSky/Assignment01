fe625432210ebffbfbcd0bf3c0ba6f0462f12773
AdenSky <n.naghieh@gmail.com>
2023-02-14 15:34:25 -0800

    End of Assignment01

--- Script.js
+++ Script.js
@@ -2,8 +2,165 @@
 // STEP 2
 // STEP 3
 
-let someMonth
-function theMonth()
-let currentMonth
-let summerMonth
-let myLibraryFunction
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+
+// // numeric literal expression
+
+// let num01=8.5
+
+// // string literal expression
+
+// let name="Neda";
+
+// // Boolean literal expression
+
+// let A=false
+
+// // null literal expression
+
+// let lastName=null;
+
+// // complex / variable expressions.
+
+// const person = {
+
+//     fullName: "Aden Sky",
+//     age: 25
+     
+// }
+
+// // Declare (but do not assign) 9 variables for the following identifiers: First Name, Last Name, Address, City, State, Zip Code, Your Age, Referral Source, May We Contact You. Use Camel Casing and Hungarian Notation when naming your identifiers.
+
+// let firstName;
+// let lastName;
+// let strAddress
+// let strCity
+// let strState
+// let intZipCode
+// let intAge
+// let referralCode
+// let strMayWeContactYou
+
+// firstName="Neda";
+// lastName="Naghieh";
+// strCity="San Diego";
+
+// Create a variable.
+
+// var numname;
+// numname=("Aden02");
+
+// console.log(numname);
+
+// Create two variables.
+
+// const people = {
+//     strName:'Aden',
+//     State:true,
+// }
+// console.log(people);
+
+// const people02 ={
+//     zipCode:92104,
+//     Snn:true,
+// }
+// console.log(people02);
+
+// 8
+
+// let someString = String ("\'Who once said, \"Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.\"\'");
+// console.log(someString);
+
+// 9
+
+// let name02
+
+// let b=null
+// console.log(name02,b);
+
+// 10
+
+// const people02 ={
+//     zipCode:92104,
+//     Snn:true,
+// }
+// console.log(typeof people02.Snn,typeof people02.zipCode);
+
+// 11
+
+// let name = prompt("Write your full name")
+// alert(`Hello ${name},welcome to the javascript class`)
+
+// // 12
+
+// let nAme;
+
+// nAme=="Neda"
+// Neda= prompt("Write your full name")
+// alert(`Hello ${Neda},welcome to the javascript class`)
+
+// 13
+// let course;
+// course=="JavaScript"
+// I didn't get what shoul I do
+
+//14
+
+// let nAme;
+// nAme=="Neda"
+
+// Neda= prompt("Write your full name")
+// alert(`Hello ${Neda}, \nWelcome to the javascript class`)
+
+// // 17
+// let X=10;
+// let Y=20;
+// let sum=X+Y;
+// console.log(sum);
+
+// // // 18
+// let X=20
+// let sum;
+// sum=X+20;
+// console.log(sum);
+
+// 19
+// let X=20
+// X*=5
+// console.log(X);
+
+// 20
+
+// let X=20;
+// let D;
+// D=20%3;
+// D/=1
+// console.log(D);
+
+// 21 & 22
+
+// let X=5
+// let Y=5
+// alert (X==Y);
+
+// let X=4
+// let Y=8
+// alert(X!=Y);
+
+// let X=4
+// let Y=8
+// alert(X>Y);
+
+// let X=4
+// let Y=8
+// alert(X===Y);
+
+// let X=4
+// let Y=8
+// alert(X!==Y);
+
